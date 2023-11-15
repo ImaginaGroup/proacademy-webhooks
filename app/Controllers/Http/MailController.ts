f2ed3a7e768509ce50b4de396b4dc543071397b0
Alex Torrenti <alex@imaginagroup.com>
2023-11-15 23:46:02 +0100

    feat: added logic to connect with webhooks

--- app/Controllers/Http/MailController.ts
+++ app/Controllers/Http/MailController.ts
@@ -1,4 +1,6 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Env from '@ioc:Adonis/Core/Env'
+import axios from 'axios'
 
 function find(obj: object, key: string) {
   const ret: any[] = []
@@ -14,13 +16,27 @@ function find(obj: object, key: string) {
 export default class MailController {
   public async store({ request, response }: HttpContextContract) {
     const body = request.body()
+    /*
     const timestamp = find(body, 'timestamp')[0]
     const transmissionId = find(body, 'transmission_id')[0]
     const type = find(body, 'type')[0]
+    */
+    const msgFrom: string = find(body, 'msg_from')[0]
 
-    if (transmissionId) {
-      console.log(timestamp, type)
+    if (msgFrom.includes(Env.get('PRODUCTION_MAIL'))) {
+      await axios.post(Env.get('PRODUCTION_WEBHOOK') + `/webhooks/syncMail`, body, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+    } else if (msgFrom.includes(Env.get('BETA_MAIL'))) {
+      await axios.post(Env.get('BETA_WEBHOOK') + `/webhooks/syncMail`, body, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
     }
-    return response.ok('transmissionId not found')
+
+    return response.ok('ok computer')
   }
 }
